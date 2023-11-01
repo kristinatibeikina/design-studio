35350a28021a60a488607625c8298607ee875b19
Kristina <kristina.tibeykina04@mail.ru>
2023-11-02 00:01:14 +0700

    Убрала не нужный URL путь

--- desingstudion/desingstudion/urls.py
+++ desingstudion/desingstudion/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('superadmin/', admin.site.urls),
-    path('', include('desingapp.urls')),
+    path('', RedirectView.as_view(url='desingapp/', permanent=True)),
+    path('desingapp/', include('desingapp.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
