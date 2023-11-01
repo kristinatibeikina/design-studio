b5e6d6affd6fdcedec4b09a61054831764d21be1
Kristina <kristina.tibeykina04@mail.ru>
2023-11-01 23:59:41 +0700

    Переделанная модель пользователя

--- desingstudion/desingapp/models.py
+++ desingstudion/desingapp/models.py
@@ -3,15 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    surname = models.CharField(max_length=254, verbose_name='Фамилия', blank=False)
-    name = models.CharField(max_length=254, verbose_name='Имя', blank=False)
-    patronymic = models.CharField(max_length=254, verbose_name='Отчество', blank=False)
+    name = models.CharField(max_length=254, verbose_name='ФИО', blank=False)
     username = models.CharField(max_length=254, verbose_name='Логин', unique=True, blank=False)
     email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False)
     password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
-
-    def full_name(self):
-        return ' '.join([self.surname, self.name, self.patronymic])
 
     def __str__(self):
         return self.full_name()
