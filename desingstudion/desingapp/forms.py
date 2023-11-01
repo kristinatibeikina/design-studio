8221c0acacc247b9459ee1376aa86e8e268d11dd
Kristina <kristina.tibeykina04@mail.ru>
2023-11-01 17:01:48 +0700

    Создали проверку ФИО

--- desingstudion/desingapp/forms.py
+++ desingstudion/desingapp/forms.py
@@ -1,10 +1,9 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 import re
 
 
-class RegistrationForm(forms.Form):
+class Registration(forms.Form):
     full_name = forms.CharField(label='ФИО', max_length=100, required=True, widget=forms.TextInput(attrs={'placeholder': 'Ваше ФИО'}))
     username = forms.CharField(label='Логин (латиница и дефис)', max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Логин'}))
     email = forms.EmailField(label='Email', required=True, widget=forms.EmailInput(attrs={'placeholder': 'example@example.com'}))
@@ -12,23 +11,9 @@ class RegistrationForm(forms.Form):
     password_confirm = forms.CharField(label='Повторите пароль', max_length=30, required=True, widget=forms.PasswordInput(attrs={'placeholder': 'Повторите пароль'}))
     agree_to_processing = forms.BooleanField(label='Согласие на обработку персональных данных', required=True)
 
-    def clean_full_name(self):
+    def check_full_name(self):
         full_name = self.cleaned_data['full_name']
         if not re.match(r'^[а-яА-Я\s-]+$', full_name):
             raise ValidationError("ФИО может содержать только кириллические буквы, дефис и пробелы.")
         return full_name
 
-    def clean_username(self):
-        username = self.cleaned_data['username']
-        if not re.match(r'^[a-zA-Z-]+$', username):
-            raise ValidationError("Логин может содержать только латиницу и дефис.")
-        if User.objects.filter(username=username).exists():
-            raise ValidationError("Пользователь с таким логином уже существует.")
-        return username
-
-    def clean_password_confirm(self):
-        password = self.cleaned_data['password']
-        password_confirm = self.cleaned_data['password_confirm']
-        if password != password_confirm:
-            raise ValidationError("Пароли не совпадают.")
-        return password_confirm
