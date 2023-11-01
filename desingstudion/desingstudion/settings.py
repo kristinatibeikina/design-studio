a3fed3ff1687e67c8b7ea83fa66d8095e223f0ac
Kristina <kristina.tibeykina04@mail.ru>
2023-11-01 13:18:54 +0700

    Переопределение порта

--- desingstudion/desingstudion/settings.py
+++ desingstudion/desingstudion/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.commands.runserver import Command as runserver
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,3 +126,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'desingapp.User'
+
+
+runserver.default_port='8080'
