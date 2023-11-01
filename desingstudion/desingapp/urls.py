58688b486aeb0456a5e49ce7cd0f984a0aabc2b8
Kristina <kristina.tibeykina04@mail.ru>
2023-11-01 17:31:32 +0700

    Путь на регистрацию

--- desingstudion/desingapp/urls.py
+++ desingstudion/desingapp/urls.py
@@ -1,1 +1,8 @@
-from django.urls import path
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('', views.base, name='base'),
+    path('cabinet/', views.cabinet, name='cabinet'),
+    path('registration/', views.registration, name='registration'),
+]
