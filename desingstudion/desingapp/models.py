c153b803e938757a093bab59c62a9aa0f46eed6d
Kristina <kristina.tibeykina04@mail.ru>
2023-11-02 16:36:56 +0700

    Создали модель заявок

--- desingstudion/desingapp/models.py
+++ desingstudion/desingapp/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.core.validators import FileExtensionValidator
 
 
 class User(AbstractUser):
@@ -9,7 +10,28 @@ class User(AbstractUser):
     password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
 
     def __str__(self):
-        return self.full_name()
+        return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=254, verbose_name='Выбор категории', blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Application(models.Model):
+    name = models.CharField(max_length=254, verbose_name='Название заявки', blank=False)
+    description = models.TextField(max_length=1000, verbose_name='Описание', blank=False)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
+    photo_file = models.ImageField(max_length=254, upload_to='images', blank=False, validators=[FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg', 'bmp'])])
+    user = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+    date = models.DateTimeField(verbose_name='Время создания заявки', auto_now_add=True)
+    status = models.CharField(max_length=254, default='Новая')
+
+    def __str__(self):
+        return f'{self.name} ({self.date})'
+
 
 
 
