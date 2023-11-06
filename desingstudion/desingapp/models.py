cf8752a5233aa699c966145f3f49e1329201b2d3
Kristina <kristina.tibeykina04@mail.ru>
2023-11-06 09:04:27 +0700

    Прописвали статусы в модели

--- desingstudion/desingapp/models.py
+++ desingstudion/desingapp/models.py
@@ -4,11 +4,15 @@ from django.core.validators import FileExtensionValidator
 from django.urls import reverse
 from django.core.exceptions import ValidationError
 
+
 class User(AbstractUser):
     name = models.CharField(max_length=254, verbose_name='ФИО', blank=False)
     username = models.CharField(max_length=254, verbose_name='Логин', unique=True, blank=False)
     email = models.CharField(max_length=254, verbose_name='Почта', unique=True, blank=False)
     password = models.CharField(max_length=254, verbose_name='Пароль', blank=False)
+
+    def __str__(self):
+        return self.user.username
 
     def __str__(self):
         return self.name
@@ -35,11 +39,19 @@ class Application(models.Model):
         if filesize > megabyte_limit * 1024 * 1024:
             raise ValidationError("Размер превышает %sMB" % str(megabyte_limit))
 
-    photo_file = models.ImageField(max_length=254, upload_to='images', blank=False,
+    photo_file = models.ImageField(max_length=254, upload_to='images/', blank=False,
                                    validators=[validate_image, FileExtensionValidator(allowed_extensions=['png', 'jpg', 'jpeg', 'bmp'])])
+
+    REQUEST_STATUS = (
+        ('Новая', 'Новая'),
+        ('Принято в работу', 'Принято в работу'),
+        ('Выполнено', 'Выполнено'),
+    )
+    status = models.CharField(max_length=254, choices=REQUEST_STATUS, default='Новая', blank=True,verbose_name="Статус")
 
     def get_absolute_url(self):
         return reverse('application_list', args=[str(self.id)])
+
 
     def __str__(self):
         return f'{self.name} ({self.date})'
