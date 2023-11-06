f93e0764137f138624f59ed0f03193e584c82a65
Kristina <kristina.tibeykina04@mail.ru>
2023-11-06 11:46:49 +0700

    Настройки

--- desingstudion/desingapp/urls.py
+++ desingstudion/desingapp/urls.py
@@ -7,8 +7,9 @@ urlpatterns = [
     path('cabinet/', views.cabinet, name='cabinet'),
     path('registration/', views.registration, name='registration'),
     path('login/', views.login, name='login'),
-    url('application_list/', views.ApplicationListView.as_view(), name='base'),
+    url('application_list/', views.ApplicationListView.as_view(), name='application_list'),
     path('logout/', views.logout, name='logout'),
     path('main_request/', views.ApplicationCreate.as_view(), name='main_request'),
     path('request/', views.MyPostListViews.as_view(), name='user_posts'),
+    path('application/<int:pk>/delete/', views.ApplicationDelete.as_view(), name='application_delete'),
 ]
--- desingstudion/desingstudion/settings.py
+++ desingstudion/desingstudion/settings.py
@@ -127,10 +127,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'desingapp.User'
 
-MEDIA_URL = 'media/'
 
-MEDIA_ROOOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media/'
 
 
 runserver.default_port = '8080'
+runserver.default_addr = 'localhost'
 
