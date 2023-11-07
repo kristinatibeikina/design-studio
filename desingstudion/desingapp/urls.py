61e1cce59b35d36f93618b58807c518aa7b55f64
Kristina <kristina.tibeykina04@mail.ru>
2023-11-07 13:25:06 +0700

    Создали путь на предсталение смены статуса

--- desingstudion/desingapp/urls.py
+++ desingstudion/desingapp/urls.py
@@ -12,4 +12,9 @@ urlpatterns = [
     path('main_request/', views.ApplicationCreate.as_view(), name='main_request'),
     path('request/', views.MyPostListViews.as_view(), name='user_posts'),
     path('application/<int:pk>/delete/', views.ApplicationDelete.as_view(), name='application_delete'),
+    path('admin_base/', views.ApplicationListViewAdmin.as_view(), name='admin_list'),
+    path('category_list/', views.CategoryListView.as_view(), name='category_list'),
+    path('category/<int:pk>/delete/', views.CategoryDelete.as_view(), name='category_delete'),
+    path('category_do/', views.CategoryCreate.as_view(), name='category_do'),
+    path('change/<int:pk>/status/', views.ChangeStatusRequest.as_view(), name='change_status'),
 ]
